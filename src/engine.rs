644937bb3594fae379a198e867d5252aab87ffce
Gergely Imreh <imrehg@gmail.com>
2018-03-30 12:08:22 +0100

    move EngineVersion type into the relevant module and fix up
    Renamed to Version, and successfully parsing MinAPIVersion var as
    well
    
    Change-type: patch

--- src/engine.rs
+++ src/engine.rs
@@ -1,21 +1,33 @@
 use errors::*;
-use types::{Client, EngineVersion};
+use types::Client;
 use network::get;
 
 use serde_json;
 
-pub fn version(client: Client) -> Result<EngineVersion> {
-    let response = get(client, "/version").chain_err(
-        || "Failed to get engine version",
-    )?;
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Version {
+    pub version: String,
+    pub os: String,
+    pub kernel_version: String,
+    pub go_version: String,
+    pub git_commit: String,
+    pub arch: String,
+    pub api_version: String,
+    #[serde(rename(deserialize = "MinAPIVersion"))]
+    pub min_api_version: String,
+    pub build_time: String,
+}
+
+pub fn version(client: Client) -> Result<Version> {
+    let response = get(client, "/version").chain_err(|| "Failed to get engine version")?;
 
     if response.status_code != 200 {
         bail!("non-200 response from server");
     }
 
-    let version: EngineVersion = serde_json::from_str(&response.body).chain_err(
-        || "Failed to deserialize engine response",
-    )?;
+    let version: Version =
+        serde_json::from_str(&response.body).chain_err(|| "Failed to deserialize engine response")?;
 
     Ok(version)
 }
